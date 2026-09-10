0fa2e50807ce11f962cf613dfab1726b16a4174d
Purvesh Kolpe <purvesh.kolpe@adypu.edu.in>
2026-09-10 14:16:18 +0530

    Fix Render server port binding

--- run_api.py
+++ run_api.py
@@ -1,2 +1,10 @@
+import os
 import uvicorn
-if __name__=='__main__': uvicorn.run('xaita_ot.api.app:app',host='127.0.0.1',port=8080,reload=False)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "xaita_ot.api.app:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", "8080")),
+        reload=False,
+    )
